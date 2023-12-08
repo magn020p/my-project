92eb8f9a750c382d360bea6fe51b79e6ff2da75a
Magnus Bolton <magnus@bolton.dk>
2023-12-08 12:23:44 +0100

    ny

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,16 +3,21 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      purple: "#3f3cbb",
       dark: {
         800: "#262525",
-        500: "#353030",
-        200: "#F6F6F6",
+        500: "#414040",
       },
-      secondary: {
+      pink: {
         800: "#FFC0CB",
-        500: "#F8F8F8",
-        200: "#B1DFF2",
+        500: "#FFD3DB",
+      },
+      blue: {
+        800: "#90D1ED",
+        500: "#B1DFF2",
+      },
+      white: {
+        800: "#F8F8F8",
+        500: "#D4D4D4",
       },
     },
     extend: {},
